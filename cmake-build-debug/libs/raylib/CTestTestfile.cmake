31b9ab98e44625bea01ba901bafc183db5a7085b
Fayxx <54201777+Fayxx@users.noreply.github.com>
2024-05-18 11:56:00 +0200

    implemented gitignore

--- cmake-build-debug/libs/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/marlonkasemann/Desktop/Coding/Projects/WormBrain/libs/raylib
-# Build directory: /Users/marlonkasemann/Desktop/Coding/Projects/WormBrain/cmake-build-debug/libs/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
